ba4a08e900be584741ef728183e5897004ba0e7b
Eng. Sultan Al Hassni <EngSayh@users.noreply.github.com>
2025-11-22 20:32:55 +0300

    chore: allowlist legacy scripts and collapse lint debt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -288,9 +288,11 @@ export default [
     rules: {
       '@typescript-eslint/no-var-requires': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
       'no-undef': 'off',
+      'no-unused-vars': 'off',
     },
   },
 ];
